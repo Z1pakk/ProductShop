7c16463f32589ae5bdb7968cfc342c60f5739267
Vlad Snow <vlad.snow@bbconsult.co.uk>
2020-04-28 21:09:58 +0300

    Add project in repository

---
+++ ProductShopLibrary/Migrations/202004281801193_Start.cs
@@ -0,0 +1,16 @@
+﻿namespace BankomatLibrary.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Start : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ ProductShopLibrary/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace BankomatLibrary.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BankomatLibrary.EFContext.EFContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(BankomatLibrary.EFContext.EFContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
